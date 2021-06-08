345c74c357021659a59ae426820523abc688f35a
eibens <l.eibensteiner@gmail.com>
2021-06-08 15:01:59 +0200

    :memo: docs: fix broken ts-doc comments

--- mod.ts
+++ mod.ts
@@ -7,7 +7,9 @@
  */
 export function modulo(n: number, m: number): number {
   return ((n % m) + m) % m;
-} /**
+}
+
+/**
  * Calculates the quotient of a number `n` modulo `m`.
  *
  * @param n is the original number.
@@ -17,14 +19,15 @@ export function modulo(n: number, m: number): number {
 
 export function quotient(n: number, m: number): number {
   return Math.floor(n / m);
-} /**
+}
+
+/**
  * Decomposes a number `n` into its quotient and modulo.
  *
  * @param n is the original number.
  * @param m is the number used for the modulo.
  * @returns the resulting quotient and modulo as a pair (in that order).
  */
-
 export const decompose = (n: number, m: number): [number, number] => {
   return [quotient(n, m), modulo(n, m)];
 };
